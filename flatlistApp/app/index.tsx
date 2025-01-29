b227d667d0e4012d9c36c19350b09b1fc44d2bc1
S5Racer <sbitrausa@gmail.com>
2025-01-29 08:19:23 -0600

    grergrf

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -4,11 +4,30 @@ import {
   StyleSheet,
   FlatList,
   TouchableOpacity,
+  TouchableHighlight,
 } from "react-native";
 import colors from "../styles/colors";
+import { useState } from "react";
 import defaultStyles from "../styles/defaultStyles";
 
 export default function Index() {
+  const [selectedId, setSelectedId] = useState<string>("1");
+    type dataType = {
+        id: string;
+        title: string;
+    }
+    const DATA: dataType[] = [
+        { id: "1", title: "ry kim" },
+        { id: "2", title: "d koik" },
+        { id: "3", title: "jay lee"},
+        { id: "4", title: "j kim" },
+    ];
+
+    const selectedList = (item: dataType) => {
+      setSelectedId(item.id);
+        console.log(item.title);
+    }
+  
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -16,7 +35,23 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList
+            data={DATA}
+            keyExtractor={(item: dataType) => item.id}
+            renderItem={({item}) => (
+              <TouchableHighlight style = {[styles.titleContainer
+              , {
+                backgroundColor: 
+                item.id === selectedId ? colors.primary : colors.secondary
+              }                
+              ]} onPress={() => selectedList(item)}>
+                <Text style={[styles.titleText,
+
+                  {color: item.id === selectedId ? colors.text.light : colors.text.dark,}
+                ]}>{item.title}</Text>
+              </TouchableHighlight>
+            )}
+          />
         </View>
       </View>
     </View>
@@ -28,6 +63,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   titleContainer: {
+    backgroundColor: colors.secondary,
     marginTop: 5,
     width: 300,
     borderTopLeftRadius: 15,
